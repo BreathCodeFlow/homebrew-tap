01ae5ed51e3698604d400a92da25bebdfd62cd63
Markus Sommer <markus.sommer@fullhaus.de>
2025-10-21 09:59:36 +0200

    chore: update tide formula to v1.2.0

--- Formula/tide.rb
+++ Formula/tide.rb
@@ -1,16 +1,16 @@
 class Tide < Formula
   desc "🌊 An opinionated macOS maintenance orchestrator with an iocraft-powered interface"
   homepage "https://github.com/BreathCodeFlow/tide"
-  version "0.1.0"
+  version "1.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/BreathCodeFlow/tide/releases/download/v#{version}/tide-aarch64-apple-darwin.tar.gz"
-      sha256 "1962d1a76a662cc2f915702499f2377a1efe7f2fd317ace1beddf12d76749bfc"
+      sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_AARCH64"
     else
       url "https://github.com/BreathCodeFlow/tide/releases/download/v#{version}/tide-x86_64-apple-darwin.tar.gz"
-      sha256 "f148dba083840c1f988c0fafdbd6e9d3584bbe20c3b037d7ae510f3b80d4ebf6"
+      sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_X86_64"
     end
   end
 
